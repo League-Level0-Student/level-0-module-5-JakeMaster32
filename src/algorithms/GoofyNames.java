6e82859e3f2fed95b99cec69f83887bc3e511a66
JakeMaster32 <46147844+JakeMaster32@users.noreply.github.com>
2019-04-29 18:31:35 -0700

    Goofy Names :smile:

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -11,26 +11,45 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String x = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
+		// Run your program to see that this works.
 
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// HINT: Use .length() to determine the number of characters in the String.
 
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
+		// 4. Create a String variable to store the next character of the name
+		// using .substring(start, end)
+		// HINT: replace 'start' and 'end' to get String with character at i
+		// 5. Use MODULO to set this variable to upper case for EVEN characters
+		// and lower case for ODD characters.
+		String y = "";
+		y = (y + x.substring(0, 1));
+		y = y.toLowerCase();
+		String z = "";
+		z = (z + x.charAt(1));
+		z = z.toUpperCase();
+		System.out.println(z);
+		y = (y + z);
+		System.out.println(y);
 
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
+		// 6. ADD this String (containing 1 char) to the goofyName String
 
-		
-		// 7. Use pop-up to show user their Goofy name
+		int a = x.length();
+		for (int b = 2; b < a; b++) {
+			if (b % 2 == 1) {
+				String d = x.charAt(b) + "";
+				y = y + d.toUpperCase();
 
+				System.out.println(y);
+			} else {String d = x.charAt(b) + "";
+				y = y + d.toLowerCase();
+			}
+
+		}
+		goofyName = goofyName + y;
+		JOptionPane.showMessageDialog(null, "Your goofy name is " + goofyName);
 	}
-}
+	// 7. Use pop-up to show user their Goofy name
 
+}
