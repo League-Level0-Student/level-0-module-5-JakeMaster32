8b5ac0a36137cd42d1891b797804ce831cf28973
League <league@iMac-2.attlocal.net>
2019-05-06 18:30:18 -0700

    Guessing pi, code4life, evilpolygons(ip) :smile:

--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -10,19 +10,40 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+Robot Ultron = new Robot();
+int a = 144;
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+Ultron.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "Choose a color", "Color Chooser", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Blue", "Green"}, 0);
 		
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
+		if(colorChoice == 0) {
+			Ultron.setPenColor(255, 0, 0);
+		}else if(colorChoice == 1) {
+			Ultron.setPenColor(0, 0, 255);
+		}else {
+			Ultron.setPenColor(0, 255, 0);
+		}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+	String x =	JOptionPane.showInputDialog("How many pentagons do you want to be drawn?");
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		int y = Integer.parseInt(x);
+		for(int z = 0; z < y; z++) {
 		//6. Make it so your shapes do not overlap
-
+		Ultron.penDown();
+		Ultron.turn(72);
+		Ultron.move(100);
+		Ultron.turn(72);
+		Ultron.move(100);
+		Ultron.turn(72);
+		Ultron.move(100);
+		Ultron.turn(72);
+		Ultron.move(100);
+		Ultron.turn(72);
+		Ultron.move(100);
+		Ultron.move(a);
+		a = a+72;
+		}
 		//7. Challenge: add more colors to the Option Dialog.
 	}
 }
