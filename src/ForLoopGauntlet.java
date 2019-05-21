13a9c001bbaecf480f536125f4eb491b221b2df1
League Student <league@iMac-56.attlocal.net>
2019-05-20 18:29:12 -0700

    for loop gauntlet :neutral_face:

--- src/ForLoopGauntlet.java
+++ src/ForLoopGauntlet.java
@@ -1,24 +1,49 @@
 
 public class ForLoopGauntlet {
-public static void main(String[] args) {
-	
-	for (int a = 0; a<100; a++) {
-		System.out.println(a);
+	public static void main(String[] args) {
+
+		for (int a = 0; a < 100; a++) {
+			System.out.println(a);
+		}
+		for (int b = 100; b > 0; b--) {
+			System.out.println(b);
+		}
+		for (int c = 2; c < 102; c = c + 2) {
+			System.out.println(c);
+		}
+		for (int d = 1; d < 100; d = d + 2) {
+			System.out.println(d);
+		}
+		for (int e = 0; e < 501; e++) {
+			if (e % 2 == 0) {
+				System.out.println(e + " is even");
+			} else {
+				System.out.println(e + " is odd");
+			}
+		}
+		for (int f = 0; f < 778; f++) {
+			if (f % 7 == 0) {
+				System.out.println(f);
+			}
+		}
+		for (int g = 0; g < 51; g++) {
+			System.out.println("In " + (1969 + g) + ", I was " + g + " years old.");
+		}
+		for (int h = 0; h < 3; h++) {
+			for (int i = 0; i < 3; i++) {
+				System.out.println("" + h + i);
+			}
+		}
+		for (int j = 1; j < 10; j += 3) {
+			System.out.println(j + " " + (j + 1) + " " + (j + 2));
+		}
+		for (int k = 0; k < 10; k++) {
+			for (int l = 1; l < 11; l++) {
+				System.out.print(k * 10 + l +" ");
+
+			}
+			System.out.println();
+			}
+
 	}
-	for (int b = 100; b>0; b--) {
-		System.out.println(b);
-	}
-	for (int c = 2; c<102; c=c+2) {
-		System.out.println(c);
-	}
-	for (int d = 1; d < 100; d=d+2) {
-		System.out.println(d);
-	}
-	for (int e = 0; e<501; e++) {
-		if(e%2 == 0) {
-			System.out.println(e+" is even");
-		}else {System.out.println(e+" is odd");}
-	}
-	for (int f = 0; f<78; f++) {}
-}
 }
